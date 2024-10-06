48a06feae3b2053305c54a4e8f6adaddf3af1a73
gaurav101203 <162018942+gaurav101203@users.noreply.github.com>
2024-10-06 19:36:27 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Role_based_login_system.settings')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,10 +15,14 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # Check if 'runserver' is in the command arguments
+    if len(sys.argv) > 1 and sys.argv[1] == 'runserver':
+        # Dynamically bind to PORT if defined as an environment variable
+        port = os.environ.get('PORT', '8000')
+        sys.argv[2:] = ['0.0.0.0:' + port]  # Set host:port to '0.0.0.0:PORT'
+
     execute_from_command_line(sys.argv)
 
-    # Dynamically bind to PORT if defined as an environment variable
-    port = os.environ.get('PORT', '8000')
-    execute_from_command_line(['manage.py', 'runserver', '0.0.0.0:' + port])
 if __name__ == '__main__':
     main()
